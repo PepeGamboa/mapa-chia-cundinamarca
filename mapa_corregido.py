6e5caaa1a5f382fddfe53e198c42fb9de4df5179
Pepe <arquitectopedrogamboa@gmail.com>
2025-09-23 19:03:38 -0500

    Mapa con centro historico corregido

---
+++ mapa_corregido.py
@@ -0,0 +1,7 @@
+﻿import folium
+
+PLAZA_DIOSA_CHIA = [4.8582, -74.0588]
+mapa = folium.Map(location=PLAZA_DIOSA_CHIA, zoom_start=15)
+folium.Marker(PLAZA_DIOSA_CHIA, popup='Plaza Diosa Chia', icon=folium.Icon(color='red')).add_to(mapa)
+mapa.save('mapa_chia_centro_corregido.html')
+print('Mapa creado')
